e03f2588c2b1f4cf19e3610e32fa2448b4224d67
Federico Pedraza <federico.pedraza4@gmail.com>
2022-12-18 16:56:50 -0300

    Added missing 404file

---
+++ src/pages/Error404.tsx
@@ -0,0 +1,13 @@
+import { Paper } from "@material-ui/core";
+import { Box } from "@mui/system";
+import { FC } from "react";
+
+const Error404 : FC = () => {
+    return (
+        <Box>
+            <Paper elevation={3}>
+                ERROR 404
+            </Paper>
+        </Box>
+    )
+}
